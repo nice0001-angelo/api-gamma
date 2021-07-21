af51bc73e316896b99146c52c1767ae50ee6d411
nice0001-angelo <nice0001@gmail.com>
2021-07-21 11:44:10 -0400

    update

---
+++ src/main/java/net/jin/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package net.jin.config;
+
+import org.springframework.security.config.annotation.web.configuration.*;
+
+import lombok.extern.slf4j.*;
+
+/**
+ * @author njh
+ *
+ */
+@Slf4j
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+}
