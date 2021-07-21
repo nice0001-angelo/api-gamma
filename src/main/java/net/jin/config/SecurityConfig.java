8b7c316d1b31115b715ae41de372d754d389cf2c
nice0001-angelo <nice0001@gmail.com>
2021-07-21 11:51:38 -0400

    update

--- src/main/java/net/jin/config/SecurityConfig.java
+++ src/main/java/net/jin/config/SecurityConfig.java
@@ -3,6 +3,7 @@
  */
 package net.jin.config;
 
+import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
 
 import lombok.extern.slf4j.*;
@@ -14,5 +15,12 @@ import lombok.extern.slf4j.*;
 @Slf4j
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception{
+		log.info("Security Configuring....");
 
+	//폼기반 인증 기능을 사용한다 이걸해야  http://localhost:8080/login 으로 redirect 되지 않는다
+	httpSecurity.formLogin();
+	}	
 }
