46937a309acb800959066a2796c2e52cf43369f1
nice0001-angelo <nice0001@gmail.com>
2021-07-22 14:30:20 -0400

    add

---
+++ src/main/java/net/jin/repository/CodeGroupRepository.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package net.jin.repository;
+
+import org.springframework.data.jpa.repository.*;
+
+import net.jin.domain.*;
+
+/**
+ * @author njh
+ *
+ */
+public interface CodeGroupRepository extends JpaRepository<CodeGroup, String>{
+	
+
+}
