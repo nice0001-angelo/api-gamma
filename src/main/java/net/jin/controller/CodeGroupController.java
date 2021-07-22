00d4233a0c65fb6ea5585ccb5d160bd06f026f5e
nice0001-angelo <nice0001@gmail.com>
2021-07-22 15:43:03 -0400

    update

--- src/main/java/net/jin/controller/CodeGroupController.java
+++ src/main/java/net/jin/controller/CodeGroupController.java
@@ -26,9 +26,9 @@ public class CodeGroupController {
 	//상세조회
 	@RequestMapping(value="{groupCode}", method=RequestMethod.GET)
 	public ResponseEntity<CodeGroup> read(@PathVariable("groupCode") String groupCode) throws Exception{
-		CodeGroup codeGroup = service.read(groupCode);
+		CodeGroup codeGroup = service.read(groupCode); //서비스에서 정한 리턴 타입을 정의 후 변수에 담는다
 		
-		return new ResponseEntity<CodeGroup>(codeGroup, HttpStatus.OK);
+		return new ResponseEntity<CodeGroup>(codeGroup, HttpStatus.OK); //변수를 리턴한다
 	}
 	
 
