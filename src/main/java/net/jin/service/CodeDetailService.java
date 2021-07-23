e84108d59874eb1f99e74798900e666c14636f3a
nice0001-angelo <nice0001@gmail.com>
2021-07-23 08:03:16 -0400

    update

--- src/main/java/net/jin/service/CodeDetailService.java
+++ src/main/java/net/jin/service/CodeDetailService.java
@@ -3,12 +3,15 @@
  */
 package net.jin.service;
 
+import org.springframework.stereotype.*;
+
 import net.jin.domain.*;
 
 /**
  * @author njh
  *
  */
+@Service
 public interface CodeDetailService {
 	
 	public CodeDetail read(String groupCode, String codeValue) throws Exception{
