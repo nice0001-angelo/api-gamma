7d2f59241e4330bf54b1c4b0c991510f37e8b26e
nice0001-angelo <nice0001@gmail.com>
2021-07-26 19:18:45 -0400

    update security config

--- src/main/java/net/jin/config/SecurityConfig.java
+++ src/main/java/net/jin/config/SecurityConfig.java
@@ -3,8 +3,12 @@
  */
 package net.jin.config;
 
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.password.*;
 
 import lombok.*;
 import lombok.extern.slf4j.*;
@@ -23,12 +27,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity httpSecurity) throws Exception{
 		log.info("Security Configuring....");
 		
-		//httpSecurity.formLogin().disable().httpBasic().disable();
+		//폼로그인기능과 베이직인증 비활성화
+		httpSecurity.formLogin().disable().httpBasic().disable();
 		
 		//CORS 사용자 설정
 		//httpSecurity.cors();
 		
-		//csrf() Disable 이걸 하지 않으면 CUD를 할수 없다
+		//csrf() Disable 이걸 하지 않으면 CUD를 할수 없다  CSRF 방지 지원 기능 비활서화
 		httpSecurity.csrf().disable();
 	}	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		
+	}
+	
+	//비밀번호 암호 처리기 생성
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		//- BCryptPasswordEncoder는 BCrypt 해싱 함수(BCrypt hashing function)를 사용해서 비밀번호를 인코딩해주는 메서드와 사용자의 의해 제출된 비밀번호와 저장소에 저장되어 있는 비밀번호의 일치 여부를 확인해주는 메서드를 제공합니다.
+		return new BCryptPasswordEncoder();
+	}
 }
