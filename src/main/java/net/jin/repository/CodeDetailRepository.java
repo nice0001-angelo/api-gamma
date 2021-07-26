094c4dca223607ce55ab62ffe55388bd2033bbc5
nice0001-angelo <nice0001@gmail.com>
2021-07-26 18:54:01 -0400

    update

--- src/main/java/net/jin/repository/CodeDetailRepository.java
+++ src/main/java/net/jin/repository/CodeDetailRepository.java
@@ -17,7 +17,7 @@ import net.jin.domain.*;
 public interface CodeDetailRepository extends JpaRepository<CodeDetail, CodeDetailId>{
 
 	@Query("SELECT max(cd.sortSeq) FROM CodeDetail cd WHERE cd.groupCode = ?1")//PQL: 해당 그룹코드중에 max sortSeq를 가져오는 쿼리
-	//public List<Object[]> getMaxSortSeq(String groupCode);
+//	public List<Object[]> getMaxSortSeq(String groupCode);
 	
 	//굳이 위에처럼 List 값으로 처리할 필요가 없다. 왜냐하면 위의 쿼리에서 나오는 값은 어짜피 유일하게 1개만 나온다.
 	public Integer getMaxSortSeq(String groupCode);
