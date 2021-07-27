3c60bed44db87886f6d10d82e39c90bff262963b
nice0001-angelo <nice0001@gmail.com>
2021-07-26 22:01:00 -0400

    added MemberController

---
+++ src/main/java/net/jin/controller/MemberController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package net.jin.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import lombok.*;
+import lombok.extern.slf4j.*;
+
+/**
+ * @author njh
+ *
+ */
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value="/users")
+public class MemberController {
+
+}
