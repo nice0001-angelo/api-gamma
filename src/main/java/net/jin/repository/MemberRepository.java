d77131f91be18751f0ce3b5caadf898d3433a0a0
nice0001-angelo <nice0001@gmail.com>
2021-07-28 11:44:56 -0400

    update

--- src/main/java/net/jin/repository/MemberRepository.java
+++ src/main/java/net/jin/repository/MemberRepository.java
@@ -6,6 +6,7 @@ package net.jin.repository;
 import java.util.*;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.*;
 
 import net.jin.domain.*;
 
@@ -14,12 +15,11 @@ import net.jin.domain.*;
  *
  */
 public interface MemberRepository extends JpaRepository<Member, Long>{
-	
-	@Query("SELECT m.userNo, m.userId, m.userPw, m.userName, cd.codeName, m.coin, m.regDate"
-			+ "FROM Member m"
-			+ "INNER JOIN CodeDetail cd ON cd.codeValue = m.job"
-			+ "INNER JOIN CodeGroup cg ON cg.groupCode = cd.groupCode"
-			+ "WHERE cg.groupCode = 'A01' ORDERY BY m.regDate DESC"
-			)
-	public List<Object[]> listAllmember();
+	@Query("SELECT m.userNo, m.userId, m.userPw, m.userName, cd.codeName, m.coin, m.regDate "
+			+ "FROM Member m "
+			+ "INNER JOIN CodeDetail cd ON cd.codeValue = m.job "
+			+ "INNER JOIN CodeGroup cg ON cg.groupCode = cd.groupCode "
+			+ "WHERE cg.groupCode = 'A01' ORDER BY m.regDate DESC")
+	public List<Object[]> listAllMember();
+
 }
