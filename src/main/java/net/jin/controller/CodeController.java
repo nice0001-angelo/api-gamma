b13e7636deadf73c1c0a6f15c6f8b8c36eab968d
nice0001-angelo <nice0001@gmail.com>
2021-08-02 14:57:15 -0400

    add CodeController

---
+++ src/main/java/net/jin/controller/CodeController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package net.jin.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import lombok.*;
+import lombok.extern.slf4j.*;
+
+/**
+ * @author njh
+ *
+ */
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value="/codes")
+public class CodeController {
+	
+
+}
