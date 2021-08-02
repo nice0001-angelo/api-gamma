ac0d467cf8b7a34526e207a8d9ecaa3628770e01
nice0001-angelo <nice0001@gmail.com>
2021-08-02 14:38:32 -0400

    add dto

---
+++ src/main/java/net/jin/dto/CodeLabelValue.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package net.jin.dto;
+
+/**
+ * @author njh
+ *
+ */
+public class CodeLabelValue {
+
+	private String value;
+	private String label;
+	
+	public CodeLabelValue() {
+		super();
+	}
+	
+	public CodeLabelValue(String value, String label) {
+		this.value = value;
+		this.label = label;
+	}
+	
+	
+}
