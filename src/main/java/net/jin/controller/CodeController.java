f167c380443bdad83449aa8e30417a8f4638cc00
nice0001-angelo <nice0001@gmail.com>
2021-08-02 15:02:10 -0400

    update

--- src/main/java/net/jin/controller/CodeController.java
+++ src/main/java/net/jin/controller/CodeController.java
@@ -3,10 +3,14 @@
  */
 package net.jin.controller;
 
+import java.util.*;
+
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.*;
 import lombok.extern.slf4j.*;
+import net.jin.dto.*;
 
 /**
  * @author njh
@@ -17,6 +21,16 @@ import lombok.extern.slf4j.*;
 @RestController
 @RequestMapping(value="/codes")
 public class CodeController {
+	
+	private final CodeService codeService;
+	
+	//코드그룹 목록조회
+	@RequestMapping(value = "/codeGroup")
+	public ResponseEntity<List<CodeLabelValue>> codeGroupList() throws Exception{
+		return new ResponseEntity<List<CodeLabelValue>>(codeService.getCodeGroupList(), HttpStatus.OK);
+	}
+		
+	
 	
 
 }
