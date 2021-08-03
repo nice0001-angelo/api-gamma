140f260ce34bd0cf9dc7b25729e5281ed2a6dec3
nice0001-angelo <nice0001@gmail.com>
2021-08-02 21:21:30 -0400

    add code method

---
+++ src/main/java/net/jin/service/CodeService.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package net.jin.service;
+
+import java.util.*;
+
+import net.jin.dto.*;
+
+/**
+ * @author njh
+ *
+ */
+public interface CodeService {
+	public List<CodeLabelValue> getCodeGroupList() throws Exception;
+
+}
---
+++ src/main/java/net/jin/service/impl/CodeSerivceImpl.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.jin.service.impl;
+
+import org.springframework.stereotype.*;
+
+import lombok.*;
+import net.jin.service.*;
+
+/**
+ * @author njh
+ *
+ */
+@RequiredArgsConstructor
+@Service
+public class CodeSerivceImpl implements CodeService{
+	
+
+}
