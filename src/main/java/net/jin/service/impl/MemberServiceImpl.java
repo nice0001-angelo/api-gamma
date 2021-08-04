02204eced31ccca0983ae8ed4c79089b098d0b9e
nice0001-angelo <nice0001@gmail.com>
2021-08-04 08:30:35 -0400

    refactoring

--- src/main/java/net/jin/service/impl/MemberServiceImpl.java
+++ src/main/java/net/jin/service/impl/MemberServiceImpl.java
@@ -25,14 +25,17 @@ public class MemberServiceImpl implements MemberService{
 	
 	private final MemberRepository memberRepository;
 	
-	//Member 전체목록 조회
+	//Member 전체목록 조회(Memeber 의 컬럼 value를 Member 구조체 형태로 record로 나열
 	public List<Member> list() throws Exception{
+		//Repository로 부터 object[] 타입으로 각각의 컬럼을 가져와서 각각의 행을 list로 반환하기 위해서 선언한다: listAllMember는 레파지토리에 선언
 		List<Object[]> valueArrays = memberRepository.listAllMember();
 		
 		List<Member> memberList = new ArrayList<Member>();
 		
+		//Memeber 구조체를 선언하여 repository로부터 가져온 값을 형변화여 add한값을 list로 리턴한다
 		for(Object[] valueArray : valueArrays) {
 			Member member = new Member();
+			
 			member.setUserNo((Long)valueArray[0]);
 			member.setUserId((String)valueArray[1]);
 			member.setUserPw((String)valueArray[2]);
@@ -40,12 +43,12 @@ public class MemberServiceImpl implements MemberService{
 			member.setJob((String)valueArray[4]);
 			member.setCoin((int)valueArray[5]);
 			member.setRegDate((LocalDateTime)valueArray[6]);
+			
 			memberList.add(member);
 		}
 		
 		
 		return memberList;
-				
 	}
 
 	//Member 상세목록 조회
