ec4c972633a660a277f091c1b3c19484e6102e0c
nice0001-angelo <nice0001@gmail.com>
2021-08-09 15:10:33 -0400

    update

--- src/main/java/net/jin/service/impl/MemberServiceImpl.java
+++ src/main/java/net/jin/service/impl/MemberServiceImpl.java
@@ -132,8 +132,7 @@ public class MemberServiceImpl implements MemberService{
 	//member에 값이 없으면 admin 값 생성
 	@Override
 	public Member setupAdmin(Member member) throws Exception{
-		
-		re
+		return memberRepository.save(member);
 	}
 	
 }
