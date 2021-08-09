2913c811084cab43dc5b143ec3980402c86d594d
nice0001-angelo <nice0001@gmail.com>
2021-08-09 05:12:38 -0400

    add

---
+++ src/main/resources/static/js/app/setup-admin.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	//등록
+	$("#adminRegisterBtn").on("click",function(){
+		var userObject = {
+			userId: $("#adminId").val(),
+			userPw: $("#adminPw").val(),
+			userName: $("#adminName").val()
+		};
+		
+		console.log("userObject: "+userObject);
+		
+		alert("userObject: "+JSON.stringify(userObject));
+		
+		$.ajax({
+			type: "POST",
+			url: "/users/setup",
+			data: JSON.stringify(userObject),
+			contextType: "application/json; charSet=UTF-8",
+			success: function(){
+				
+				alert("Admin user Created");
+				
+			},
+			error: function(xhr, status, error){
+				alert("code: "+xhr.status+"\n"
+						+ "message: "+xhr.responseText+"\n"
+						+ "error: "+error);
+			}
+		});
+	});
+	
+	//입력값 리셋
+		$("#adminResetBtn").on("click",function(){
+			$("#adminId").val("");
+			$("#adminPw").val("");
+			$("#adminName").val("");
+		});
+});
