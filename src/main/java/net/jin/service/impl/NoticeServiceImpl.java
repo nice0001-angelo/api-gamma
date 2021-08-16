3ea9aed8e35d778a20fdf380dcc31c033501d9c6
nice0001-angelo <nice0001@gmail.com>
2021-08-16 05:45:52 -0400

    refactoring

--- src/main/java/net/jin/service/impl/NoticeServiceImpl.java
+++ src/main/java/net/jin/service/impl/NoticeServiceImpl.java
@@ -27,7 +27,7 @@ public class NoticeServiceImpl implements NoticeService{
 	//전체목록조회
 	@Override
 	public List<Notice> list() throws Exception{
-		return noticeRepository.findAll(Sort.by(Direction.DESC,"noticeNO"));
+		return noticeRepository.findAll(Sort.by(Direction.DESC,"noticeNo"));
 	}
 	
 	//상세목록조회
