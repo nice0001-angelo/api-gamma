b77d0cbade0f0a4c118bb4f03a3b55eb8fccf4a7
nice0001-angelo <nice0001@gmail.com>
2021-08-16 04:16:54 -0400

    define domain repository

---
+++ src/main/java/net/jin/repository/NoticeRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package net.jin.repository;
+
+import org.springframework.data.jpa.repository.*;
+
+import net.jin.domain.*;
+
+/**
+ * @author njh
+ *
+ */
+public interface NoticeRepository extends JpaRepository<Notice, Long>{
+
+}
