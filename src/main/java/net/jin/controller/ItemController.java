cd70a418279efb7683cf440516cd1a1bb16a30ea
nice0001-angelo <nice0001@gmail.com>
2021-08-18 10:08:28 -0400

    update

--- src/main/java/net/jin/controller/ItemController.java
+++ src/main/java/net/jin/controller/ItemController.java
@@ -53,12 +53,25 @@ public class ItemController {
 			@RequestPart("file2") MultipartFile previewImageFile) throws Exception{
 		
 		//Item타입의 item 변수에 객체주입하면서 초기화
-		//Convert "JSON" to "Java Object" 할때 사용하는것이 아래타입의 문장임.(String to Object)
+		//Convert "JSON" to "Java Object" 할때 사용하는것이 아래타입의 문장임.(String to Object), JSON file 이나 JSON url을 convert 할때는 문장이 달라짐 
 		Item item = new ObjectMapper().readValue(itemString, Item.class);
 		
 		String itemName = item.getItemName();
 		String description = item.getDescription();
 		
+		//itemName 의 Null 체크
+		if(itemName != null) {
+			item.setItemName(itemName);
+		}
+		
+		//description 의 Null 체크
+		if(description != null) {
+			item.setDescription(description);
+		}
+		
+		//item object에 file 세팅
+		item.setPicture(originalImageFile);
+		item.setPreview(previewImageFile);
 	}
 	
 	//삭제
