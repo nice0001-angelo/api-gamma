246ae0aaa4c8e0f5ca05293b6512c15bba4f7b0d
nice0001-angelo <nice0001@gmail.com>
2021-09-29 22:34:18 -0400

    update

--- src/main/java/net/jin/service/impl/ItemServiceImpl.java
+++ src/main/java/net/jin/service/impl/ItemServiceImpl.java
@@ -81,7 +81,7 @@ public class ItemServiceImpl implements ItemService{
 	@Override
 	public String getPreview(Long itemId) throws Exception{
 		Item item = itemRepository.getById(itemId);
-
+	
 		
 		return item.getPreviewUrl();	
 
