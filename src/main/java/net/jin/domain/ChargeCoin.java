2f4bbdb638e4122eef4759c2216e490ca53f458a
nice0001-angelo <nice0001@gmail.com>
2021-11-22 02:04:41 -0500

    update

--- src/main/java/net/jin/domain/ChargeCoin.java
+++ src/main/java/net/jin/domain/ChargeCoin.java
@@ -3,10 +3,25 @@
  */
 package net.jin.domain;
 
+import javax.persistence.*;
+
+import lombok.*;
+
 /**
  * @author njh
  *
  */
+@Setter
+@Getter
+@EqualsAndHashCode(of="historyNo")
+@ToString
+@Entity
+@Table(name="charge_coin_history")
 public class ChargeCoin {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long historyNo;
+	
+	private Long userNo;
 }
