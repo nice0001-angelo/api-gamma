bfe3c966dc3bb237efdd1f452882d672629d989f
nice0001-angelo <nice0001@gmail.com>
2021-11-24 00:10:24 -0500

    create CoinController, Serivce, ServiceImpl

--- src/main/java/net/jin/controller/CoinController.java
+++ src/main/java/net/jin/controller/CoinController.java
@@ -3,10 +3,17 @@
  */
 package net.jin.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import lombok.*;
+
 /**
  * @author njh
  *
  */
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/coins")
 public class CoinController {
 
 }
