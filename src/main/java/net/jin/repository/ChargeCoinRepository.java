7e543b4209807a6ae8b9e1455381b64b67148ec3
nice0001-angelo <nice0001@gmail.com>
2021-11-23 21:43:48 -0500

    create ChargeCoinRepository interface

---
+++ src/main/java/net/jin/repository/ChargeCoinRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package net.jin.repository;
+
+import org.springframework.data.jpa.repository.*;
+
+import net.jin.domain.*;
+
+/**
+ * @author njh
+ *
+ */
+public interface ChargeCoinRepository extends JpaRepository<ChargeCoin, Long>{
+
+}
