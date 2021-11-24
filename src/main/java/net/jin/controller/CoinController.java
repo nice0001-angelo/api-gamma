72970a55bf1b08056b24849885b58c4ff1ddb63f
nice0001-angelo <nice0001@gmail.com>
2021-11-24 00:17:36 -0500

    update

--- src/main/java/net/jin/controller/CoinController.java
+++ src/main/java/net/jin/controller/CoinController.java
@@ -3,9 +3,15 @@
  */
 package net.jin.controller;
 
+import org.springframework.context.*;
+import org.springframework.http.*;
+import org.springframework.security.access.prepost.*;
+import org.springframework.security.core.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.*;
+import net.jin.common.security.domain.*;
+import net.jin.service.*;
 
 /**
  * @author njh
@@ -15,5 +21,16 @@ import lombok.*;
 @RestController
 @RequestMapping("/coins")
 public class CoinController {
+	
+	private final CoinService coinService;
+	
+	private final MessageSource messageSource;
+	
+	//코인충전 처리
+	@PreAuthorize("hasRole('MEMBER')")
+	@PostMapping(value = "/charge/{amount}", produces = "text/plain;charset=UTF-8")
+	public ResponseEntity<String> charge(@PathVariable("amount") int amount, @AuthenticationPrincipal CustomUser customUser) throws Exception {
+		return "String";
+	}
 
 }
