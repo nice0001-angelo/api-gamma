89aa7fd6f2c879f6ab383a169008996adcd3e66d
nice0001-angelo <nice0001@gmail.com>
2021-11-28 01:35:20 -0500

    update

--- src/main/java/net/jin/service/CoinService.java
+++ src/main/java/net/jin/service/CoinService.java
@@ -3,6 +3,9 @@
  */
 package net.jin.service;
 
+import java.util.*;
+
+import net.jin.common.security.domain.*;
 import net.jin.domain.*;
 
 /**
@@ -13,4 +16,7 @@ public interface CoinService {
 	
 	//코인충전처리
 	public void charge(ChargeCoin chargeCoin) throws Exception;
+	
+	//코인화면처리
+	public List<ChargeCoin> list(Long userNo) throws Exception;
 }
