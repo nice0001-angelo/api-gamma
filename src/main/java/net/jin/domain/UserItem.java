8b91a51c4c0282764f95e605170889f29c560a9c
nice0001-angelo <nice0001@gmail.com>
2021-11-29 23:58:14 -0500

    update Domain Class

--- src/main/java/net/jin/domain/UserItem.java
+++ src/main/java/net/jin/domain/UserItem.java
@@ -3,7 +3,13 @@
  */
 package net.jin.domain;
 
+import java.time.*;
+
 import javax.persistence.*;
+
+import org.hibernate.annotations.*;
+
+import com.fasterxml.jackson.annotation.*;
 
 import lombok.*;
 
@@ -22,4 +28,28 @@ public class UserItem {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userItemNo;
+	
+	private Long userNo;
+	
+	private Long itemId;
+	
+	@Transient
+	private String itemName;
+	
+	@Transient
+	private Integer price;
+	
+	@Transient
+	private String description;
+	
+	@Transient
+	private String pictureUrl;
+	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+	@CreationTimestamp
+	private LocalDateTime regDate;
+	
+	@JsonFormat(pattern = "yyyy-MM dd HH:mm")
+	@UpdateTimestamp
+	private LocalDateTime updDate;
 }
