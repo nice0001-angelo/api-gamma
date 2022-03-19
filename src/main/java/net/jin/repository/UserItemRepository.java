953263fab0cef7db9ac740b94bf34673ba3753e0
nice0001-angelo <nice0001@gmail.com>
2022-03-19 01:54:00 -0400

    refactoring

---
+++ src/main/java/net/jin/repository/UserItemRepository.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package net.jin.repository;
+
+import org.springframework.data.jpa.repository.*;
+
+import net.jin.domain.*;
+
+/**
+ * @author njh
+ *
+ */
+public interface UserItemRepository extends JpaRepository<UserItem, Long> {
+	
+
+}
