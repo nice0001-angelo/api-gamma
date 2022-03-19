9d2527f57a83a62d605eba27ca17f62a482f8b59
nice0001-angelo <nice0001@gmail.com>
2022-03-19 01:25:28 -0400

    refactoring

--- src/main/java/net/jin/domain/PayCoin.java
+++ src/main/java/net/jin/domain/PayCoin.java
@@ -19,8 +19,6 @@ import lombok.*;
  * @author njh
  *
  */
-@Getter
-@Setter
 //롭복에서 값의 내용이 같은지 그리고 객체가 같은지 비교하는 equals와 hashcode를 생성해주는 어노테이션
 @EqualsAndHashCode(of = "historyNo")
 @ToString
@@ -47,4 +45,62 @@ public class PayCoin {
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm")
 	@UpdateTimestamp
 	private LocalDateTime updDate;
+
+	public Long getHistoryNo() {
+		return historyNo;
+	}
+
+	public void setHistoryNo(Long historyNo) {
+		this.historyNo = historyNo;
+	}
+
+	public Long getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(Long userNo) {
+		this.userNo = userNo;
+	}
+
+	public Long getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(Long itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public LocalDateTime getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(LocalDateTime regDate) {
+		this.regDate = regDate;
+	}
+
+	public LocalDateTime getUpdDate() {
+		return updDate;
+	}
+
+	public void setUpdDate(LocalDateTime updDate) {
+		this.updDate = updDate;
+	}
+	
+	
 }
--- src/main/java/net/jin/domain/UserItem.java
+++ src/main/java/net/jin/domain/UserItem.java
@@ -19,8 +19,6 @@ import lombok.*;
  * @author njh
  *
  */
-@Setter
-@Getter
 @EqualsAndHashCode(of = "userItemNo")
 @ToString
 @Entity
@@ -54,4 +52,78 @@ public class UserItem {
 	@JsonFormat(pattern = "yyyy-MM dd HH:mm")
 	@UpdateTimestamp
 	private LocalDateTime updDate;
+
+	public Long getUserItemNo() {
+		return userItemNo;
+	}
+
+	public void setUserItemNo(Long userItemNo) {
+		this.userItemNo = userItemNo;
+	}
+
+	public Long getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(Long userNo) {
+		this.userNo = userNo;
+	}
+
+	public Long getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(Long itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPictureUrl() {
+		return pictureUrl;
+	}
+
+	public void setPictureUrl(String pictureUrl) {
+		this.pictureUrl = pictureUrl;
+	}
+
+	public LocalDateTime getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(LocalDateTime regDate) {
+		this.regDate = regDate;
+	}
+
+	public LocalDateTime getUpdDate() {
+		return updDate;
+	}
+
+	public void setUpdDate(LocalDateTime updDate) {
+		this.updDate = updDate;
+	}
+	
+	
 }
