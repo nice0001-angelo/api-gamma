99180439eb5625d4f8da3a505271b2849aa89a71
nice0001-angelo <nice0001@gmail.com>
2022-03-22 02:00:34 -0400

    refactoring

--- src/main/java/net/jin/repository/PayCoinRepository.java
+++ src/main/java/net/jin/repository/PayCoinRepository.java
@@ -13,16 +13,10 @@ import net.jin.domain.*;
  * @author njh
  *
  */
-public interface PayCoinRepository extends JpaRepository<PayCoin, Long>{
-	/*
-	 * @Query("select a.historyNo, a.userNo, a.itemId, a.itemName, a.amount, a.regDate"
-	 * + "from PayCoin a inner join Item b on a.itemId = b.itemId" +
-	 * "where a.historyNo > 0 and a.userNo = ?1" + "order by a.historyNo DESC")
-	 * public List<Object[]> listPayHistory(Long userNo);
-	 */
+public interface PayCoinRepository extends JpaRepository<PayCoin, Long> {
 	
 	@Query("SELECT a.historyNo, a.userNo, a.itemId, b.itemName, a.amount, a.regDate "
-			+ "FROM PayCoin a INNER JOIN Item b ON a.itemId = b.itemId "
+			+ "FROM PayCoin a INNER JOIN Item b ON a.itemId = b.itemId " 
 			+ "WHERE a.historyNo > 0 AND a.userNo = ?1 "
 			+ "ORDER BY a.historyNo DESC")
 	public List<Object[]> listPayHistory(Long userNo);
