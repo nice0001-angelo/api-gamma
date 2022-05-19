c32207c6371ee0f2f2293898396c003ebae9a2df
nice0001-angelo <nice0001@gmail.com>
2022-05-19 00:41:43 -0400

    update

--- src/main/java/net/jin/repository/UserItemRepository.java
+++ src/main/java/net/jin/repository/UserItemRepository.java
@@ -1,32 +1,4 @@
-///**
-// * 
-// */
-//package net.jin.repository;
-//
-//import java.util.*;
-//
-//import org.springframework.data.jpa.repository.*;
-//import org.springframework.stereotype.*;
-//
-//import net.jin.domain.*;
-//
-///**
-// * @author njh
-// *
-// */
-//public interface UserItemRepository extends JpaRepository<UserItem, Long> {
-//	
-//	public List<UserItem> findByUserNo(Long userNo);
-//	
-//	
-//	@Query("Select a.userItemNo, a.userNo, a.itemId, a.regDate, b.itemName, b.price, b.description, b.pictureUrl"
-//			+ "from UserItem a INNER JOIN Item b ON a.itemId = b.itemId"
-//			+ "where a.userItemNo = ?1")
-//	public List<Object[]> listUserItem(Long userNo);
-//}
-
-
-//ckage net.jin.repository;
+package net.jin.repository;
 
 import java.util.*;
 
