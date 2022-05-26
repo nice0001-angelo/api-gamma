aecd21a53d5312cbfc1dcb2ddb6d306bc49dbbd5
nice0001-angelo <nice0001@gmail.com>
2022-05-26 01:44:28 -0400

    upload

---
+++ src/main/java/net/jin/common/util/UploadFileUtils.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.jin.common.util;
+
+import lombok.extern.slf4j.*;
+
+/**
+ * @author njh
+ *
+ */
+@Slf4j
+public class UploadFileUtils {
+	
+	public static String uploadFile(String uploadPath, String originalPath, byte[] fileData) throws Exception{
+		
+		return uploadedFileName;
+	}
+
+}
