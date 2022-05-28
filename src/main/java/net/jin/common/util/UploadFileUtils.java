0b175edc2f8d33e8c06aa54bb3bbc77d736d28a3
nice0001-angelo <nice0001@gmail.com>
2022-05-28 01:04:55 -0400

    update

--- src/main/java/net/jin/common/util/UploadFileUtils.java
+++ src/main/java/net/jin/common/util/UploadFileUtils.java
@@ -3,6 +3,10 @@
  */
 package net.jin.common.util;
 
+import java.util.*;
+
+import org.springframework.util.*;
+
 import lombok.extern.slf4j.*;
 
 /**
@@ -12,9 +16,23 @@ import lombok.extern.slf4j.*;
 @Slf4j
 public class UploadFileUtils {
 	
-	public static String uploadFile(String uploadPath, String originalPath, byte[] fileData) throws Exception{
+	public static String uploadFile(String uploadPath, String originName, byte[] fileData) throws Exception{
 		
+		UUID uid = UUID.randomUUID();
+		
+		String saveName = uid.toString()+"_"+originName;
+		
+		String savedPath = calcPath(uploadPath);
+		
+		File target = new File(uploadPath+savedPath, saveName);
+		
+		FileCopyUtils.copy(fileData, target);
+		
+		String uploadedFileName = makeuploadedFileName(uploadPath, savedPath, saveName);
+				
 		return uploadedFileName;
 	}
+	
+	
 
 }
