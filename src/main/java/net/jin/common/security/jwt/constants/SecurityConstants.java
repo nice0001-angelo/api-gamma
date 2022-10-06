cc1c74a23be992845fb86bb2ae68bd7b2351a7e7
nice0001-angelo <nice0001@gmail.com>
2022-10-05 23:38:01 -0400

    security

--- src/main/java/net/jin/common/security/jwt/constants/SecurityConstants.java
+++ src/main/java/net/jin/common/security/jwt/constants/SecurityConstants.java
@@ -12,9 +12,33 @@ public final class SecurityConstants {
 	//로그인 인증 URL 정의
 	public static final String AUTH_LOGIN_URL="/api/authenticate";
 	
+	//Signing key for HS512 algorithm
+	//You can use the page http://www.allkeysgenerator.com/ to generate all kinds of keys
+	public static final String JWT_SCRET = "";
+	
 	//JWT 토큰 기본 상수값 정의
 	public static final String TOKEN_HEADER = "Authorization";
 	public static final String TOKEN_PREFIX = "Bearer";
 	public static final String TOKEN_TYPE = "JWT";
+	public static final String TOKEN_ISSUER = "secure-api";
+	public static final String TOKEN_AUDIENCE = "secure-app";
+	public static final String TOKEN_ALG = "HS512";
+	
+	//Username
+	public static final String USER_MP = "raptor_memportal";
+	public static final String USER_EP = "raptor_empportal";
+	public static final String USER_AM = "raptor_snow";
+	public static final String USER_DV = "raptor_dev";
+	
+	public static final String MP ="MP";
+	public static final String EP ="EP";
+	public static final String AM ="AM";
+	public static final String IT ="IT";
+	public static final String DV ="DV";
+	public static final String SW ="Swagger UI";
 
+	private SecurityConstants() {
+		throw new IllegalStateException("Cannot create instance of static util class");
+	}
 }
+
