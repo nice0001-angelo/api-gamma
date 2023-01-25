8102abfa8e3910277e2fadd4b3d1fcb5260fed94
nice0001-angelo <nice0001@gmail.com>
2023-01-25 14:26:40 -0500

    1st git commit test with eclipse

--- src/main/java/net/jin/config/SecurityConfig.java
+++ src/main/java/net/jin/config/SecurityConfig.java
@@ -26,7 +26,7 @@ import net.jin.common.security.jwt.filter.*;
 import net.jin.common.security.jwt.provider.*;
 
 /**
- * @author njh
+ * @author njh add
  *
  */
 @Slf4j
@@ -35,103 +35,98 @@ import net.jin.common.security.jwt.provider.*;
 @EnableWebSecurity
 //시큐리티 애노테이션 활성활르 위한 옵션
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
-	//JWT 토큰을 제공하는 JwtTokenProvider 필드 선언
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	// JWT 토큰을 제공하는 JwtTokenProvider 필드 선언
 	private final JwtTokenProvider jwtTokenProvider;
-	
+
 	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception{
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		log.info("Security Configuring....");
-		
-		//폼로그인기능과 베이직인증 비활성화
+
+		// 폼로그인기능과 베이직인증 비활성화
 //		httpSecurity.formLogin().disable().httpBasic().disable();
-		
-		//CORS 사용자 설정
+
+		// CORS 사용자 설정
 		httpSecurity.cors();
-		
-		//csrf() Disable 하지 않으면 CUD를 할수 없다  CSRF 방지 지원 기능 비활성화
+
+		// csrf() Disable 하지 않으면 CUD를 할수 없다 CSRF 방지 지원 기능 비활성화
 		httpSecurity.csrf().disable();
-		
-		//JWT 관련 필터 보안 컨텍스트에 추가
-		httpSecurity.addFilterAt(new JwtAuthenticationFilter(authenticationManager(), jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-		.addFilterBefore(new JwtRequestFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		
-		//웹경로 보안 설정
-		httpSecurity.authorizeRequests()
-		.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-		.antMatchers("/").permitAll()
-		.antMatchers("/authenticate/**").access("permitAll")
-		.antMatchers("/codes/**").access("permitAll")
-		.antMatchers("/users/**").access("permitAll")
-		.antMatchers("/codegroups/**").access("hasRole('ADMIN')")
-		.antMatchers("/codedetails/**").access("hasRole('ADMIN')")
-		//회원게시판 웹경로 보안 권한 지정
-		.antMatchers("/boards/**").access("request.method == 'GET' ? permitAll : hasAnyRole('ADMIN', 'MEMBER')")
-		//공지사항 웹경로 보안 권한 지정
-		.antMatchers("/notices/**").access("request.method == 'GET' ? permitAll : hasRole('ADMIN')")
-		//상품관리 웹경로 보안 권한 지정
-		.antMatchers("/items/**").access("request.method == 'GET' ? permitAll : hasRole('ADMIN')")
-		//코인충전 웹 경로 보안 지정
-		.antMatchers("/coins/**").access("hasRole('MEMBER')")
-		//구매상품 웹 보안 경로 지정
-		.antMatchers("/useritems/**").access("hasAnyRole('ADMIN','MEMBER')")
-		//공개자료 웹 경로 보안 지정
-		.antMatchers("/pds/**").access("request.method == 'GET' ? permeitALL : hasRole('ADMIN')")
-		//Swagger 경로 보안 지정(permitAll())
-		.antMatchers("/v2/api-docs",
-				     "/v2/api-docs/**",
-				     "/swagger-resources/**",
-				     "/swagger-ui.html/**",
-				     "/swagger*/**").permitAll()
-		.anyRequest().authenticated();
-	
-	}	
-	
-	//CustomUserDetailsService 빈을 인증 제공자에게 지정하고 비밀번호 암호처리기를 등록한다
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-		auth.userDetailsService(customUserDetailsService())
-		.passwordEncoder(passwordEncoder());
+
+		// JWT 관련 필터 보안 컨텍스트에 추가
+		httpSecurity
+				.addFilterAt(new JwtAuthenticationFilter(authenticationManager(), jwtTokenProvider),
+						UsernamePasswordAuthenticationFilter.class)
+				.addFilterBefore(new JwtRequestFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		// 웹경로 보안 설정
+		httpSecurity.authorizeRequests().requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+				.permitAll().antMatchers("/").permitAll().antMatchers("/authenticate/**").access("permitAll")
+				.antMatchers("/codes/**").access("permitAll").antMatchers("/users/**").access("permitAll")
+				.antMatchers("/codegroups/**").access("hasRole('ADMIN')").antMatchers("/codedetails/**")
+				.access("hasRole('ADMIN')")
+				// 회원게시판 웹경로 보안 권한 지정
+				.antMatchers("/boards/**").access("request.method == 'GET' ? permitAll : hasAnyRole('ADMIN', 'MEMBER')")
+				// 공지사항 웹경로 보안 권한 지정
+				.antMatchers("/notices/**").access("request.method == 'GET' ? permitAll : hasRole('ADMIN')")
+				// 상품관리 웹경로 보안 권한 지정
+				.antMatchers("/items/**").access("request.method == 'GET' ? permitAll : hasRole('ADMIN')")
+				// 코인충전 웹 경로 보안 지정
+				.antMatchers("/coins/**").access("hasRole('MEMBER')")
+				// 구매상품 웹 보안 경로 지정
+				.antMatchers("/useritems/**").access("hasAnyRole('ADMIN','MEMBER')")
+				// 공개자료 웹 경로 보안 지정
+				.antMatchers("/pds/**").access("request.method == 'GET' ? permeitALL : hasRole('ADMIN')")
+				// Swagger 경로 보안 지정(permitAll())
+				.antMatchers("/v2/api-docs", "/v2/api-docs/**", "/swagger-resources/**", "/swagger-ui.html/**",
+						"/swagger*/**")
+				.permitAll().anyRequest().authenticated();
+
 	}
-	
-	//비밀번호 암호 처리기 생성
+
+	// CustomUserDetailsService 빈을 인증 제공자에게 지정하고 비밀번호 암호처리기를 등록한다
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(customUserDetailsService()).passwordEncoder(passwordEncoder());
+	}
+
+	// 비밀번호 암호 처리기 생성
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		//- BCryptPasswordEncoder는 BCrypt 해싱 함수(BCrypt hashing function)를 사용해서
-		//비밀번호를 인코딩해주는 메서드와 사용자의 의해 제출된 비밀번호와 저장소에 저장되어 있는 비밀번호의 일치 여부를 확인해주는 메서드를 제공합니다.
+		// - BCryptPasswordEncoder는 BCrypt 해싱 함수(BCrypt hashing function)를 사용해서
+		// 비밀번호를 인코딩해주는 메서드와 사용자의 의해 제출된 비밀번호와 저장소에 저장되어 있는 비밀번호의 일치 여부를 확인해주는 메서드를
+		// 제공합니다.
 		return new BCryptPasswordEncoder();
 	}
-	
-	//스프링 시큐리티의 UserDetailsService를 구현한 클래스를 빈으로 등록한다
+
+	// 스프링 시큐리티의 UserDetailsService를 구현한 클래스를 빈으로 등록한다
 	@Bean
 	public UserDetailsService customUserDetailsService() {
 		return new CustomUserDetailsService();
 	}
-	
-	//Cors 사용자설정
+
+	// Cors 사용자설정
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
-	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    
-	    CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
-        config.setExposedHeaders(Arrays.asList("Authorization","Content-Disposition"));
-        
-	    source.registerCorsConfiguration("/**", config);
-	    System.out.println();
-	    
-	    return source;
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		config.setExposedHeaders(Arrays.asList("Authorization", "Content-Disposition"));
+
+		source.registerCorsConfiguration("/**", config);
+		System.out.println();
+
+		return source;
 	}
 }
--- src/main/java/net/jin/controller/BoardController.java
+++ src/main/java/net/jin/controller/BoardController.java
@@ -26,53 +26,54 @@ import net.jin.service.*;
 @RestController
 @RequestMapping(value = "/boards")
 public class BoardController {
-	
+
 	private final BoardService boardService;
-	
-	//전체목록 조회
+
+	// 전체목록 조회
 	@RequestMapping(value = "", method = RequestMethod.GET)
-	public ResponseEntity<List<Board>> list() throws Exception{
+	public ResponseEntity<List<Board>> list() throws Exception {
+		System.out.println("1st test for committing git");
 		return new ResponseEntity<List<Board>>(boardService.list(), HttpStatus.OK);
 	}
-	
-	//상세목록조회
+
+	// 상세목록조회
 	@RequestMapping(value = "/{boardNo}", method = RequestMethod.GET)
-	public ResponseEntity<Board> read(@PathVariable("boardNo") Long boardNo) throws Exception{
+	public ResponseEntity<Board> read(@PathVariable("boardNo") Long boardNo) throws Exception {
 		return new ResponseEntity<Board>(boardService.read(boardNo), HttpStatus.OK);
 	}
-	
-	
-	//등록
-	//@PreAuthorize("hasRole('MEMBER')")
+
+	// 등록
+	// @PreAuthorize("hasRole('MEMBER')")
 	@RequestMapping(value = "", method = RequestMethod.POST)
-	public ResponseEntity<Board> insert(@Validated @RequestBody Board board, @AuthenticationPrincipal CustomUser customUser) throws Exception{
-		//customerUser로 부터 UserId를 가져다가 board 변수에 담고
+	public ResponseEntity<Board> insert(@Validated @RequestBody Board board,
+			@AuthenticationPrincipal CustomUser customUser) throws Exception {
+		// customerUser로 부터 UserId를 가져다가 board 변수에 담고
 		board.setWriter(customUser.getUserId());
-		//등록하기 위해 boardService를 호출한다
+		// 등록하기 위해 boardService를 호출한다
 		boardService.insert(board);
 
-		//리턴 결과는 insert한 내용을 읽어온다 
+		// 리턴 결과는 insert한 내용을 읽어온다
 		return new ResponseEntity<Board>(boardService.read(board.getBoardNo()), HttpStatus.OK);
 	}
-	
-	
-	//삭제
-	//@PreAuthorize("hasRole('MEMBER' and principal.username == #writer) or hasRole('ADMIN')")
+
+	// 삭제
+	// @PreAuthorize("hasRole('MEMBER' and principal.username == #writer) or
+	// hasRole('ADMIN')")
 	@RequestMapping(value = "/{boardNo}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable Long boardNo, @RequestParam("writer") String writer) throws Exception{
+	public ResponseEntity<Void> delete(@PathVariable Long boardNo, @RequestParam("writer") String writer)
+			throws Exception {
 		boardService.delete(boardNo);
 		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
-	
-	
-	//수정
-	//@PreAuthorize("hasRole('MEMBER' and principal.username == #board.writer) or hasRole('ADMIN')")
+
+	// 수정
+	// @PreAuthorize("hasRole('MEMBER' and principal.username == #board.writer) or
+	// hasRole('ADMIN')")
 	@RequestMapping(value = "/{boardNo}", method = RequestMethod.PUT)
-	public ResponseEntity<Board> update(@PathVariable("boardNo") Long boardNo, @Validated @RequestBody Board board) throws Exception{
-		
+	public ResponseEntity<Board> update(@PathVariable("boardNo") Long boardNo, @Validated @RequestBody Board board)
+			throws Exception {
+
 		return new ResponseEntity<Board>(boardService.update(board), HttpStatus.OK);
 	}
-	
-	
 
 }
